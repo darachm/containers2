181fa10cae5760643671f4a963cdd49e35ee60b7
darachm <nope>
2021-03-02 11:30:55 -0800

    fixing order of installs, need common first

--- r/Singularity.r
+++ r/Singularity.r
@@ -18,14 +18,11 @@ MAINTAINER darachm
     echo 'export DEBIAN_FRONTEND=noninteractive' >> $SINGULARITY_ENVIRONMENT
     ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
-    add-apt-repository 'deb [arch=amd64,i386] https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-
     apt-get -qq update
     apt-get -qq install apt-transport-https software-properties-common
 
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
     add-apt-repository 'deb [arch=amd64,i386] https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-
     apt-get -qq update
 
     apt-get -qq install r-base r-base-dev
