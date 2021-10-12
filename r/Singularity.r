ca299d7b46045711037cf1f8bbdcf82087380588
darachm <nope>
2021-10-12 15:30:21 -0700

    added different r packages now, so its layered by what goes in, but still just two layer on the extras

--- r/Singularity.r
+++ r/Singularity.r
@@ -18,25 +18,21 @@ MAINTAINER darachm
     echo 'export DEBIAN_FRONTEND=noninteractive' >> $SINGULARITY_ENVIRONMENT
     ln -fs /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
-    apt-get -qq update
-    apt-get -qq install apt-transport-https software-properties-common
+    apt-get update
+    apt-get -y upgrade
+    apt-get -y install --no-install-recommends apt-transport-https software-properties-common dirmngr wget
 
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-    add-apt-repository 'deb [arch=amd64,i386] https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-    apt-get -qq update
+    #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+    #add-apt-repository 'deb [arch=amd64,i386] https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+    apt-get install --no-install-recommends software-properties-common dirmngr
+    wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+    add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+    apt-get update
 
-    apt-get -qq install r-base r-base-dev
-    apt-get -qq install vim gnupg2 git wget g++ gcc libxml2-dev libssl-dev \
+    apt-get -y install --no-install-recommends r-base r-base-dev \
+        vim gnupg2 git g++ gcc libxml2-dev libssl-dev \
         curl libcurl4-openssl-dev pandoc \
         libfontconfig1-dev libgit2-dev
 
-    apt-get -qq upgrade
-
-    Rscript -e 'install.packages("tidyverse",dependencies=T);'
-    Rscript -e 'install.packages("magrittr",dependencies=T);'
-    Rscript -e 'install.packages("devtools",dependencies=T);'
-
 %test
 
-    Rscript -e 'library(tidyverse)'
-    Rscript -e 'installed.packages();'
